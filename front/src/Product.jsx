cec0fc37cc17d8cc768c8ab07449467bec1ef47a
juned <mo.junedbadi@gmail.com>
2023-01-26 22:38:28 +0530

    juned

--- front/src/Product.jsx
+++ front/src/Product.jsx
@@ -7,7 +7,7 @@ export default function Product() {
   const [product, setProduct] = useState([])
   const [checked, setChecked] = useState([]);
   console.log('hii');
-  console.log('master');
+  console.log('juned');
   const getAllProducts = () => {
     axios.get('http://127.0.0.1:8000/api/product').then((res) => {
       //console.log(res.data)
